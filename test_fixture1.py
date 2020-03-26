02b5053cf4b01e4f84270f63f7f63c894318a1b9
Anastasia <anonikaa@gmail.com>
2020-03-26 15:07:30 +0300

    add pytest fixture and yield

--- test_fixture1.py
+++ test_fixture1.py
@@ -1,44 +1,29 @@
+import pytest
 from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-
-class TestMainPage1(): #это тест-сюьт
-
-    @classmethod
-    def setup_class(self): #создали экземпляр браузера один раз
-        print("\nstart browser for test suite..")
-        self.browser = webdriver.Chrome()
-
-    @classmethod
-    def teardown_class(self): #закрыли браузер один раз
-        print("quit browser for test suite..")
-        self.browser.quit()
-
-    #здесь тесты
-    def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector("#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+#задали область покрытия только в классах
+@pytest.fixture(scope="class")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser #дойдя до класса передаём инициализированную browser, после исполнения тестов, возвращаемся к
+    print("\nquit browser..") #части кода после yield
+    browser.quit()
 
 
-class TestMainPage2(): #здесь для каждого теста открывался и закрывался браузер
+class TestMainPage1():
 
-    def setup_method(self):
-        print("start browser for test..")
-        self.browser = webdriver.Chrome()
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        print("start test1")
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        print("finish test1")
 
-    def teardown_method(self):
-        print("quit browser for test..")
-        self.browser.quit()
-
-    def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector("#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print("start test2")
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+        print("finish test2")
